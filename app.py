83f61e409714ac9a57c05c44e6a86fcf4b731a71
jyuuuk <jasonyu0100@gmail.com>
2019-01-10 11:02:23 +1100

    Added weather, greet and base routes

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello World"
+
+@app.route('/greet')
+def greet():
+    name = request.values.get('name')
+    return f"Hello {name}!"
+
+@app.route('/weather')
+def weather():
+    temp = int(request.values.get('temp'))
+    if temp > 30:
+        return f"Ooo that's hot. That's hot"
+    else:
+        return f"The temperature is {temp}"
+
+
+if __name__ == '__main__':
+    app.run()
